ce9305410483397ba5793863dc81355dcc3f9114
syazany97 <a162016@siswa.ukm.edu.my>
2018-10-22 23:29:34 +0800

    Adding database migrations for book

---
+++ database/migrations/2018_10_12_123020_create_seats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->increments('busID');
+            $table->string('seatNo');
+            $table->string('seatTaken');
+            $table->string('seatAvail');
+
+
+
+           // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seats');
+    }
+}
