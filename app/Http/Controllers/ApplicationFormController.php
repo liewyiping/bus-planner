f446cbd854775585eb1562aedda3e6a17e6029af
bezane97 <syazany1997@gmail.com>
2018-11-07 20:54:20 +0800

    Created application form model controller and migration for application form

---
+++ app/Http/Controllers/ApplicationFormController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace busplannersystem\Http\Controllers;
+
+use busplannersystem\ApplicationForm;
+use Illuminate\Http\Request;
+
+class ApplicationFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \busplannersystem\ApplicationForm  $applicationForm
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ApplicationForm $applicationForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \busplannersystem\ApplicationForm  $applicationForm
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ApplicationForm $applicationForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \busplannersystem\ApplicationForm  $applicationForm
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ApplicationForm $applicationForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \busplannersystem\ApplicationForm  $applicationForm
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ApplicationForm $applicationForm)
+    {
+        //
+    }
+}
