61d8b3cc16d311729eea26dc212edcdf67ae7967
liewyiping96 <A160719@siswa.ukm.edu.my>
2018-11-13 20:55:46 +0800

    Update database/migrations/2014_10_12_000000_create_users_table.php
    Changing the attributes name to small letters

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('user_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::update("ALTER TABLE users AUTO_INCREMENT = 10000001;");
     }
 
     /**
