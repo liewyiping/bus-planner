b0070676b0ee4d80897806bb5db25e4a59a9135e
bezane97 <syazany1997@gmail.com>
2018-11-27 07:40:11 +0800

    Move create ApplicationForm function from controller to model

--- app/ApplicationForm.php
+++ app/ApplicationForm.php
@@ -3,6 +3,7 @@
 namespace busplannersystem;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class ApplicationForm extends Model
 {
@@ -15,6 +16,68 @@ class ApplicationForm extends Model
     ];
  
     protected $table = 'application_forms';
+
+    public function create(Request $request){
+
+         //Handle resume upload
+         if($request->hasFile('operator_resume'))
+        
+         {
+ 
+             $resumeWithExt=$request -> file('operator_resume')->getClientOriginalName();
+ 
+         // Get the full file name
+             $resumefilename = pathinfo($resumeWithExt,PATHINFO_FILENAME);            
+ 
+         //Get the extension file name
+             $resumeextension = $request ->file('operator_resume')-> getClientOriginalExtension();
+         //File name to store
+         $resumefileNameToStore=$resumefilename.'_'.time().'.'.$resumeextension;
+         
+         //Upload Pdf file
+         $path =$request ->file('operator_resume')->storeAs('public/operator_resume',$resumefileNameToStore);
+         
+         }
+             else{
+                 $resumefileNameToStore = 'noPDF.pdf';
+             }
+
+         //Handle license upload
+         
+         if($request->hasFile('operator_license'))
+ 
+             {
+     
+                 $licenseWithExt=$request -> file('operator_license')->getClientOriginalName();
+     
+             // Get the full file name
+                 $licensefilename = pathinfo($licenseWithExt,PATHINFO_FILENAME);            
+     
+             //Get the extension file name
+                 $licenseextension = $request ->file('operator_license')-> getClientOriginalExtension();
+             //File name to store
+             $licensefileNameToStore=$licensefilename.'_'.time().'.'.$licenseextension;
+             
+             //Upload Pdf file
+             $path =$request ->file('operator_license')->storeAs('public/operator_license',$licensefileNameToStore);
+             
+             }
+                 else{
+                     $licensefileNameToStore = 'noPDF.pdf';
+                 }
+
+     //Create a new application
+     $application_forms = new ApplicationForm();
+     $application_forms -> name = $request -> input('name');
+     $application_forms -> email = $request -> input('email');
+     $application_forms -> company_name = $request -> input('company_name');          
+     $application_forms -> operator_resume = $resumeWithExt;
+     $application_forms -> operator_resume_link = $resumefileNameToStore;
+     $application_forms -> operator_license =  $licenseWithExt;
+     $application_forms -> operator_license_link = $licensefileNameToStore;  
+     $application_forms -> save();
+     
+    }
  
 
     public function user(){
