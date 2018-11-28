8a398ee78d8ded2bf384740fe05fcee76702dab7
bezane97 <syazany1997@gmail.com>
2018-11-28 20:14:09 +0800

    Redirecting from creating trip to creating seats: Masih problem illegal offset type

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -61,22 +61,28 @@ class TripController extends Controller
 
         ]);
 
+        $bus_id=$request ->  input('bus_id');
+        $trips = new Trip();
+        $trips -> bus_id =  $bus_id;           
+        $trips -> route_id = $request ->  input('route_id');  
+        $trips -> date_depart = $request ->  input('date_depart');  
+        $trips -> time_depart = $request ->  input('time_depart');  
+         $trips -> ticket_price = $request ->  input('ticket_price');
+        $trips -> save();
 
-        $trip = new Trip();
-        $trip->create($request);
 
-        $trip_id=$trip->trip_id;
+        $trip_id=$trips->trip_id;
         $totseat=45; //Assuming bus has 45 seats standard.
-        $busID=$trip->bus_id;
+        $busID=$trips->bus_id;
 
-        $bus_id_find=Bus::find($busID);
-       // $bus_layout= $bus_id_find->total_seat;
-        $bus_layout = DB::table('buses')->where('bus_id', $bus_id_find)->pluck('total_seat');
+    
+         $bus_layout = DB::table('buses')->where('bus_id', $busID)->pluck('total_seat');
+         return redirect('seat.createlist')->with($trip_id)->with($totseat)->with($bus_layout);
 
-        
+        //return $trip_id;
+        // return $bus_layout;
 
-        
-        return redirect('seat.createlist')->with($trip_id)->with($totseat)->with($bus_layout);
+
 
         //return redirect('operator/insert-trip-info');
 
--- app/Trip.php
+++ app/Trip.php
@@ -18,16 +18,6 @@ class Trip extends Model
 
     public function create(Request $request){
 
-        //Create trip
-        $bus_id=$request ->  input('bus_id');
-        $trips = new Trip();
-        $trips -> bus_id =  $bus_id;           
-        $trips -> route_id = $request ->  input('route_id');  
-        $trips -> date_depart = $request ->  input('date_depart');  
-        $trips -> time_depart = $request ->  input('time_depart');  
-         $trips -> ticket_price = $request ->  input('ticket_price');
-        $trips -> save();
-
 
 
 
