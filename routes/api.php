9adb98aa5ff71676668fcd41aaab4e7dd68bcec5
izleenfaqihah <izleenfaqihah@gmail.com>
2018-11-28 16:58:07 +0800

    payment

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/payment', function () {
+    // Set your secret key: remember to change this to your live secret key in production
+	// See your keys here: https://dashboard.stripe.com/account/apikeys
+	\Stripe\Stripe::setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+	// Token is created using Checkout or Elements!
+	// Get the payment token ID submitted by the form:
+	$token = $_POST['stripeToken'];
+	$charge = \Stripe\Charge::create([
+	    'amount' => 999,
+	    'currency' => 'usd',
+	    'description' => 'Example charge',
+	    'source' => $token,
+	]);
+
+	print_r("PAYMENT SUCCESSFUL!");
 });
