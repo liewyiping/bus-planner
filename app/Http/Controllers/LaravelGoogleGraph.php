21f99f7c1713d9479b7510b9f98624bff062968f
liewyiping96 <A160719@siswa.ukm.edu.my>
2018-12-28 03:23:42 +0800

    Line chart for popular date/month

--- app/Http/Controllers/LaravelGoogleGraph.php
+++ app/Http/Controllers/LaravelGoogleGraph.php
@@ -4,6 +4,7 @@ namespace busplannersystem\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use busplannersystem\Ticket;
 
 class LaravelGoogleGraph extends Controller
 {
@@ -22,4 +23,23 @@ class LaravelGoogleGraph extends Controller
      }
      return view('analytics.google_pie_chart')->with('company_name', json_encode($array));
     }
+
+    ////////////////////line graph part ///////////////////////////////////////////////////////
+    function index_popular_date_line_graph()
+    {
+        $ticket = DB::table('tickets')
+                    ->select(
+                        DB::raw("(DATE_FORMAT(date_depart,'%M, %Y')) as date"),
+                        DB::raw("sum(pax_num) as number"))
+                    ->groupBy(DB::raw("YEAR(date_depart)"), DB::raw("MONTH(date_depart)"))
+                    ->get();
+                    
+        $result[] = ['Month','Number'];
+        foreach ($ticket as $key => $value) {
+            $result[++$key] = [$value->date, $value->number];
+        }
+
+        return view('analytics.popular_date_line_chart')->with('popular_month', json_encode($result));
+      }
+
 }
---
+++ resources/views/analytics/popular_date_line_chart.blade.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      var popular_month = <?php echo $popular_month; ?>;
+      console.log(popular_month);
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(popular_month);
+        var options = {
+          title: 'Popular month',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+        var chart = new google.visualization.LineChart(document.getElementById('linechart'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="linechart" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
