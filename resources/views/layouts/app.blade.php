c8e002ce993ec28a47ccb295f34b4e665e09ab74
liewyiping96 <A160719@siswa.ukm.edu.my>
2018-12-30 14:27:25 +0800

    Pls migrate table users only

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,9 @@
                                 @endif
                             </li>
                         @else
-                            <li class="nav-item dropdown">
+                            @if (Auth::user()->role == 'customer')
+                            <li class="links" style="padding: 12px;"><a>Point: {{ Auth::user()->point }}</a></li>
+                            <li class="links nav-item dropdown" style="margin:15px;">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     <strong>{{ Auth::user()->name }}<strong> <span class="caret"></span>
                                 </a>
@@ -122,6 +124,25 @@
                                     </form>
                                 </div>
                             </li>
+                            @else
+                            <li class="links nav-item dropdown" style="margin:15px;">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <strong>{{ Auth::user()->name }}<strong> <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endif
                         @endguest
                     </ul>
                 </div>
