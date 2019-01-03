d53058700ee5ae92dbace9b458d827af78ee6d35
joanneKate <joanne.lww5@gmail.com>
2019-01-03 14:46:50 +0800

    chart move to admin
    chart move to admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,6 +19,9 @@
                         <a href="{{ url('/admin/view-new-operator-application') }}">
                         <input type="button" class="btn btn-primary" value="View Operator Application"/>
                         </a>
+                        <a href="{{ url('/laravel_google_chart') }}">
+                        <input type="button" class="btn btn-primary" value="Popular Bus Company"/>
+                        </a>
                     </div>
                     <br>
                     </div>
